73d1005e0a67b8501492585b7df9ad5c7e33da18
Ralph Joedel <joedel.rf@gmail.com>
2024-09-28 22:03:28 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo 'Project Title : Eco and Sustainable Product Marketplace' ."<br>";
+echo 'Leader : Ralph Joedel Fonte' ."<br>";
+echo 'Members :  Roice Panes, Arron Catapang' ."<br>";
+
+
+?>
