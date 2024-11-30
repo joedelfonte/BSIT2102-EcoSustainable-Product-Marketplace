5c8da24c8f97b48808dad030b35611c9c68ba2eb
Ralph Joedel <joedel.rf@gmail.com>
2024-11-30 13:30:02 +0800

    Add files via upload

---
+++ src/database/users/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('userCrud.php');
+
+$editData = new Users();
+$editData->read();
+
+
+?>
---
+++ src/database/users/userCrud.php
@@ -0,0 +1,31 @@
+<?php
+require_once realpath(dirname(__FILE__) . '../../../../config.php');
+require_once(ROOT_PATH .'\src\database\database.php');
+
+class Users extends Database {
+
+    function __construct(){
+        parent::__construct();
+        //echo 'Database is Set';
+    }
+
+    //add ka na lang ng crud 
+    //ito ang read
+    function read(){
+        $sqlQuery = "Select * from users;";
+        $result = $this->conn->query($sqlQuery);
+        $row = $result->fetchall();
+        echo count($row);
+
+        if (count($row) > 0) {
+            var_dump($row);
+            return $row;
+        } else {
+            echo 'No results';
+            $row = null;
+        }
+    }
+}
+
+
+?>
